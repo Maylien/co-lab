e46a09b83d29c631aab6e6f36f8204bebed3ed9f
Maylien <littlemaylien@gmail.com>
2019-11-08 20:09:20 +0900

    first commit

---
+++ pages/index.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <h1>Hello World!</h1>
+    <NLink to="/about">
+      About Page
+    </NLink>
+    <br>
+    <NLink to="/myPage">
+      My Page
+    </NLink>
+  </div>
+</template>
+
+<script>
+import Logo from '~/components/Logo.vue'
+import VuetifyLogo from '~/components/VuetifyLogo.vue'
+
+export default {
+  components: {
+    Logo,
+    VuetifyLogo
+  }
+}
+</script>
---
+++ pages/myPage.vue
@@ -0,0 +1,8 @@
+<template>
+    <div>
+        <h1>About page</h1>
+        <NLink to="/">
+            Top page
+        </NLink>
+    </div>
+</template>
